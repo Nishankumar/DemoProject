b7f950c9ab1236d3853c5c610775b64d0b5b3b96
NishanKumar <nishankumarvgm@gmail.com>
2022-01-23 10:22:50 +0530

    All commit

--- DemoGitProject/DemoGitProject/Program.cs
+++ DemoGitProject/DemoGitProject/Program.cs
@@ -10,6 +10,7 @@ namespace DemoGitProject
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome");
             Console.WriteLine("Hello World!...");
         }
     }
